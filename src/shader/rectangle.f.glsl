80fba33bf7259131682c7d42637c2e14e4f5f341
Kevin Balz <kevin.balz@web.de>
2016-05-11 13:00:39 +0200

    Rectangle shader refactoring

--- src/shader/rectangle.f.glsl
+++ src/shader/rectangle.f.glsl
@@ -1,7 +1,7 @@
 #version 110
 
-varying vec3 fcolor;
+uniform vec3 color;
 
 void main() {
-    gl_FragColor = vec4(fcolor, 1.0);
+    gl_FragColor = vec4(color, 1.0);
 }
--- src/shader/rectangle.v.glsl
+++ src/shader/rectangle.v.glsl
@@ -2,13 +2,9 @@
 
 attribute vec2 position;
 
-uniform vec3 color;
 uniform mat4 projection;
 uniform mat4 model;
 
-varying vec3 fcolor;
-
 void main() {
     gl_Position = projection * model * vec4(position, 0.0, 1.0);
-    fcolor = color;
 }
